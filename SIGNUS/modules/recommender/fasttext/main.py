69bb78f8215deb9f8542992051b891651dc4550f
IML <shin10256@gmail.com>
2020-10-11 18:28:42 +0900

    모델 테스트 케이스 모듈 작성

--- SIGNUS/modules/recommender/fasttext/main.py
+++ SIGNUS/modules/recommender/fasttext/main.py
@@ -1,46 +1,6 @@
-from modules.recommender.fasttext.trainer import Trainer
-from pprint import pprint
+from modules.recommender.fasttext import Recommender
 
-sent_1 = [
-    ['computer', 'aided', 'design'],
-    ['computer', 'science'],
-    ['computational', 'complexity'],
-    ['military', 'supercomputer'],
-    ['central', 'processing', 'unit'],
-    ['onboard', 'car', 'computer'],
-]
+recommender = Recommender("./ft/soojle_ft_model")
 
-sent_2 = [
-    ['computer', 'design', 'aided'],
-    ['computer', 'scienc'],
-    ['I', 'love', 'him'],
-    ['military', 'supercomputer'],
-    ['I', 'love', 'you'],
-]
-
-
-trainer = Trainer()
-trainer.set_params(
-    vec_size=10,
-    windows=3,
-    min_count=1,
-    iteration=10,
-    workers=1
-)
-pprint(trainer.get_params())
-
-trainer.set_corpora(sent_1)
-trainer.train()
-
-print(trainer.model)
-
-trainer.save_model(path="./test_model")
-trainer.load_model(path="./test_model")
-
-trainer.set_corpora(sent_2)
-trainer.update()
-
-print(trainer.model)
-
-
-
+print(recommender.doc2words("python"))
+recommender.make_test_report()
